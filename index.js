a6729a908a5815ac89292a8f017e0489b62f73df
MattGibbard <matt.gibbard@hotmail.com>
2018-10-05 18:24:43 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Add requires
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const chalk = require('chalk');
 
 const app = express();
@@ -18,6 +19,42 @@ app.set('view engine', 'ejs');
 
 //Homepage render
 app.get('/', function(req, res) {
+    res.render('pages/home.ejs');
+});
+
+//Find route
+app.post('/find', function(req, res) {
+    //Should format the postcode to remove spaces etc
+    res.redirect('/find/' + req.body.postcode);
+});
+
+//Postcode render
+app.get('/find/:postcode', function(req, res) {
+    //Save postcode parameter in URL
+    var urlParamPostcode = (req.params.postcode);
+    console.log(chalk.blue(urlParamPostcode));
+
+    //Need to show onscreen loader at this point
+
+    //Need to add postcode validity checker here.
+    //api.postcodes.io/postcodes/[postcode]/validate
+
+    request('https://api.postcodes.io/postcodes/' + urlParamPostcode, function (error, response, body) {
+        postcodeAPIResponse = JSON.parse(body);
+        //console.log('error:', error);
+        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body);
+
+        console.log(postcodeAPIResponse.result.quality);
+        console.log(postcodeAPIResponse.result.longitude);
+        console.log(postcodeAPIResponse.result.latitude);
+
+    });
+
+    //Need to figure our which local business service to use.
+    // https://developers.zomato.com/api
+    // https://developers.google.com/places/web-service/search#PlaceSearchRequests
+
     res.render('pages/home.ejs');
 });
 
